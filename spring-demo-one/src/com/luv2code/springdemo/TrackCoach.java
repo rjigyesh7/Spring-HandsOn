34245a324c515744e2ca3a14922b280c68329b3a
rjigyesh7 <rtdjigyesh777@gmail.com>
2020-10-20 08:33:49 +0530

    Added TrackCoach.java

---
+++ spring-demo-one/src/com/luv2code/springdemo/TrackCoach.java
@@ -0,0 +1,10 @@
+package com.luv2code.springdemo;
+
+public class TrackCoach implements Coach {
+
+	@Override
+	public String getDailyWorkout() {
+		return "Run few more rounds of 5KM each ";
+	}
+
+}
